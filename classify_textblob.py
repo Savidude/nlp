a11389e11223973fa0c9d8701673a32aa7d8b192
Shukhrat Mirrakhimov <shux121995@gmail.com>
2022-10-17 21:06:01 +0300

    Add requirements.txt, clear classify_textblob and add .gitignore

--- classify_textblob.py
+++ classify_textblob.py
@@ -4,6 +4,7 @@ import csv
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
 
+
 # import nltk
 # nltk.download('movie_reviews')
 
@@ -13,17 +14,18 @@ def categorize_sentiment(sentiment):
     else:
         return 0
 
+
 reviews = pd.read_csv("data/Restaurant_Reviews.tsv", sep="\t")
 
-with open('data/derived/restaurant_reviews_textblob.csv', 'w', newline = '') as file:
-   writer = csv.writer(file, delimiter="\t")
-   writer.writerow(["Review", "Liked", "Category"])
+with open('data/derived/restaurant_reviews_textblob.csv', 'w', newline='') as file:
+    writer = csv.writer(file, delimiter="\t")
+    writer.writerow(["Review", "Liked", "Category"])
 
-   for i, review in reviews.iterrows():
-       text = review['Review']
-       blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
-       sentiment = blob.sentiment
-       category = categorize_sentiment(sentiment.classification)
+    for i, review in reviews.iterrows():
+        text = review['Review']
+        blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+        sentiment = blob.sentiment
+        category = categorize_sentiment(sentiment.classification)
 
-       writer.writerow([text, review['Liked'], category])
-       print(text, " -> ", sentiment.classification)
+        writer.writerow([text, review['Liked'], category])
+        print(text, " -> ", sentiment.classification)
